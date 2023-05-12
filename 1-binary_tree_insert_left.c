609bc4176459c628680826195946880a4020c12f
kundi21 <cundi.machado@gmail.com>
2023-05-12 11:46:22 -0700

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,12 +11,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *newNode = NULL;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 
 	newNode = binary_tree_node(parent, value);
 
 	if (!newNode)
-		return NULL;
+		return (NULL);
 
 	if (parent->left)
 	{
@@ -25,6 +25,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 
 	parent->left = newNode;
-
-	return newNode;
+	return (newNode);
 }
