cf849817a828c396ba0b14780ca90a4ad009f567
kundi21 <cundi.machado@gmail.com>
2023-05-15 05:20:14 -0700

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes an entire binary tree
+ * @tree - the binary tree.
+**/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *left_child, *right_child;
+
+	if (tree)
+	{
+		left_child = tree->left;
+		right_child = tree->right;
+		free(tree);
+		binary_tree_delete(left_child);
+		binary_tree_delete(right_child);
+	}
+}
